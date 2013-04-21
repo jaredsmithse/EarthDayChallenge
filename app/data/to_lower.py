aba90fb7ca531c386b879bd736161784fab6c67a
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2013-04-21 01:19:31 -0400

    Add new data set, the new number crunching just killed my laptop!

---
+++ app/data/to_lower.py
@@ -0,0 +1,20 @@
+f = open("climate_change.csv")
+
+state_map = open("../state-name-map.csv")
+
+state_list = {}
+
+for line in state_map:
+	line = line.split(",")
+	state_list[line[1].strip()] = line[0].strip()
+
+print state_list
+index = 0
+for line in f:
+	if index == 0:
+		index += 1
+		continue
+	state = line[0:2]
+	new_state = state_list[state]
+	line = new_state + line[2:].lower()
+	print line.strip()
--- app/server.R
+++ app/server.R
@@ -2,9 +2,47 @@ library(shiny) # Load shiny
 library(ggplot2) # Fancy Plots
 library(maps) # Map Data
 
+
 poly.county <- map_data("county")
 
-air.quality <- read.csv("data/air-quality-2010.csv")
+climate.change <- read.csv("./data/fixed_climate_data.csv")
+
+#climate.change$subregion <- gsub("\\.", "", climate.change$subregion)
+
+joined <- merge(poly.county, climate.change, by = c("region", "subregion"), all.x = TRUE)
+
+joined <- joined[order(joined$order), ] # Fix merge dicking around with ordering
+
+p <- ggplot() + guides(fill=FALSE) +
+  geom_polygon(
+    data = joined,
+    aes(
+      x = long,
+      y = lat,
+      group = group,
+      fill = subregion), # Color is one-per-region
+    colour = "white"  # Ouline color is white
+  )
+
+print(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#County data for air quality plotting
+poly.county <- map_data("county")
+
+air.quality <- read.csv("./data/air-quality-2010.csv")
 
 air.quality$subregion <- gsub("\\.", "", air.quality$subregion)
 
@@ -21,7 +59,7 @@ shinyServer(function(input, output) {
           x = long,
           y = lat,
           group = group,
-          fill = region), # Color is one-per-region
+          fill = subregion), # Color is one-per-region
         colour = "white"  # Ouline color is white
       )
     
