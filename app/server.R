0a58520131bdd088dec2d6d7c5969fc083077feb
Brian To <bxt5647@rit.edu>
2013-04-21 05:07:02 -0400

    Stuff that kinda maybe works.
    Merging between map counties and air quality data janked beyond help.

--- app/server.R
+++ app/server.R
@@ -2,19 +2,24 @@ library(shiny) # Load shiny
 library(ggplot2) # Fancy Plots
 library(maps) # Map Data
 
+poly.county <- map_data("county")
 
-poly.state <- map_data("state")
+air.quality <- read.csv("app/data/air-quality-2010.csv")
+
+air.quality$subregion <- gsub("\\.", "", air.quality$subregion)
+
+joined <- merge(poly.county, air.quality, by = c("region"), all.x = TRUE)
 
 shinyServer(function(input, output) {
   output[["map"]] <- renderPlot({
-    p <- ggplot() +
+    p <- ggplot() + guides(fill=FALSE) +
       geom_polygon(
-        data = poly.state,
+        data = joined,
         aes(
           x = long,
           y = lat,
           group = group,
-          fill = region), # Color is one-per-region
+          fill = pm2.5.wtd.am.microg.by.m3), # Color is one-per-region
         colour = "white"  # Ouline color is white
       )
     
